bbc1052dae15643a6268e0b65d59b65dea0a9e94
gatu_teddy <59992213+Retard-the-old@users.noreply.github.com>
2024-05-23 06:07:24 +0300

    scrollview

--- screens/pages/appointment.js
+++ screens/pages/appointment.js
@@ -1,56 +1,107 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 function Appointment() {
     return (
-        <View>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-            <Text>Hello appointments</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+            </View>
+            <View>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+            </View>
+            <View>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+                <Text>Hello appointments</Text>
+            </View>
+
+            <View>
+                <View>
+                    <Text>Check out what else we do: </Text>
+                </View>
+
+                <ScrollView
+                    horizontal={true}
+                    contentContainerStyle={{ paddingHorizontal: 10 }}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <View style={[styles.container]}>
+
+                        <View style={[styles.forecastDays]}>
+                            <Image source={require('../../assets/servFeatIcon1.png')}
+                                style={{ height: 110, width: 110, resizeMode: 'center' }} />
+                            <Text >hello</Text>
+                            <Text >Hello too</Text>
+                        </View>
+                        <View style={[styles.forecastDays]}>
+                            <Image source={require('../../assets/servFeatIcon2.png')}
+                                style={{ height: 110, width: 110, resizeMode: 'center' }} />
+                            <Text >hello</Text>
+                            <Text >Hello too</Text>
+                        </View>
+                        <View style={[styles.forecastDays]}>
+                            <Image source={require('../../assets/servFeatIcon3.png')}
+                                style={{ height: 110, width: 110, resizeMode: 'center' }} />
+                            <Text >hello</Text>
+                            <Text >Hello too</Text>
+                        </View>
+                        <View style={[styles.forecastDays]}>
+                            <Image source={require('../../assets/servFeatIcon4.png')}
+                                style={{ height: 110, width: 110, resizeMode: 'center' }} />
+                            <Text >hello</Text>
+                            <Text >Hello too</Text>
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
+            
+        </SafeAreaView>
 
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+    },
+    forecastDays: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 5,
+        borderRadius: 12,
+        paddingVertical: 6,
+        backgroundColor: '#4dab48'
+    }
+
+});
 
 export default Appointment;
